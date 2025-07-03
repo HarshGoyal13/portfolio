07bc81c58ad1fe62d90f4b5bec90a9c756fad353
Harsh Goyal <harshgoyal0219@gmail.com>
2025-07-03 12:27:53 +0530

    Update CV

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ " Blockcahin Developer, MERN-Stack Developer","Software Developer", "Web Designer", ];
+  const toRotate = [ " Blockchain Developer, MERN-Stack Developer","Software Developer", "Web Designer", ];
   const period = 2000;
 
   useEffect(() => {
